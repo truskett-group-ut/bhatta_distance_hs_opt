5aa01ac8a3c99bdbae76292892d9f9845cf01460
rjadrich <rjadrich@gmail.com>
2018-01-19 09:04:48 -0600

    xyz reader and cell list analysis test code done

---
+++ setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+
+
+#from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize("fib.pyx"),
+)
